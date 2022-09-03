04e0c16ffe4bb696e07dd58681ad448f7ce354a9
신희제(Heejae Shin) <wufniks@gmail.com>
2022-09-03 14:01:05 +0900

    add comment on methods of TextChunk

--- src/rewritable_units/tokens/text_chunk.rs
+++ src/rewritable_units/tokens/text_chunk.rs
@@ -22,42 +22,61 @@ impl PyTextChunk {
 
 #[pymethods]
 impl PyTextChunk {
+    /// Returns the textual content of the chunk.
     #[inline]
     pub fn as_str(&self) -> &str {
         self.0.as_str()
     }
 
+    /// Returns the type of the text in the chunk.
+    ///
+    /// The type of the text depends on the surrounding context of the text. E.g. regular visible
+    /// text and text inside a `<script>` element will have different types. Refer to [`TextType`]
+    /// for more information about possible text types.
     #[inline]
     pub fn text_type(&self) -> PyTextType {
         PyTextType(self.0.text_type())
     }
 
+    /// Returns `true` if the chunk is last in a HTML text node.
+    ///
+    /// Note that last chunk can have empty textual content.
     #[inline]
     pub fn last_in_text_node(&self) -> bool {
         self.0.last_in_text_node()
     }
 
+    /// Inserts `content` before the text chunk.
+    ///
+    /// Consequent calls to the method append `content` to the previously inserted content.
     #[inline]
     pub fn before(&mut self, content: &str, content_type: PyContentType) {
         self.0.before(content, content_type.into())
     }
 
+    /// Inserts `content` after the text chunk.
+    ///
+    /// Consequent calls to the method prepend `content` to the previously inserted content.
     #[inline]
     pub fn after(&mut self, content: &str, content_type: PyContentType) {
         self.0.after(content, content_type.into())
     }
 
+    /// Replaces the text chunk with the `content`.
+    ///
+    /// Consequent calls to the method overwrite previous replacement content.
     #[inline]
     pub fn replace(&mut self, content: &str, content_type: PyContentType) {
         self.0.after(content, content_type.into())
     }
 
-    /// Removes the end tag.
+    /// Removes the text chunk.
     #[inline]
     pub fn remove(&mut self) {
         self.0.remove()
     }
 
+    /// Returns `true` if the text chunk has been replaced or removed.
     #[inline]
     pub fn removed(&self) -> bool {
         self.0.removed()
